809bfb411f06247e10d454b8cfd03d9062d4b04f
Anastasia <nastya70sv@mail.ru>
2022-11-29 12:17:32 +0300

    Добавили HW_3

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine());
+
+int [] arr = new int [elementsCount];
+
+for(int i = 0; i < arr.Length; i++)
+{
+Console.Write($"Введите элемент массива под индексом {i}: ");
+arr[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива: ");
+Console.Write('[' + string.Join(", ", arr) + ']');
